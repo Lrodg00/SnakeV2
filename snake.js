d5cde41b579e047a1f7911e8d3745695ab6d6d93
Lrodg00 <77740668+Lrodg00@users.noreply.github.com>
2021-04-13 17:20:02 -0400

    changed file name and made modifications

--- snake.js
+++ snake.js
@@ -1,16 +1,16 @@
 (function() {
-  var SIZE = 500; // Size of the play-field in pixels
+  var SIZE = 600; // Size of playing field
   var GRID_SIZE = SIZE / 50;
   var c = document.getElementById('c');
-  c.height = c.width = SIZE * 2; // 2x our resolution so retina screens look good
+  c.height = c.width = SIZE * 2;
   c.style.width = c.style.height = SIZE + 'px';
   var context = c.getContext('2d');
-  context.scale(2, 2); // Scale our canvas for retina screens
+  context.scale(2, 2); 
 
-  var direction = newDirection = 1; // -2: up, 2: down, -1: left, 1: right
-  var snakeLength = 5;
-  var snake = [{x: SIZE / 2, y: SIZE / 2}]; // Snake starts in the center
-  var candy = null;
+  var direction = newDirection = 1; 
+  var snakeLength = 3;
+  var snake = [{x: SIZE / 2, y: SIZE / 2}]; 
+  var food = null;
   var end = false;
 
   function randomOffset() {
@@ -24,62 +24,62 @@
   function tick() {
     var newHead = {x: snake[0].x, y: snake[0].y};
 
-    // Only change directon if the new direction is a different axis
+    
     if (Math.abs(direction) !== Math.abs(newDirection)) {
       direction = newDirection;
     }
-    var axis = Math.abs(direction) === 1 ? 'x' : 'y'; // 1, -1 are X; 2, -2 are Y
+    var axis = Math.abs(direction) === 1 ? 'x' : 'y'; 
     if (direction < 0) {
       newHead[axis] -= GRID_SIZE; // Move left or down
     } else {
       newHead[axis] += GRID_SIZE; // Move right or up
     }
 
-    // Did we eat a candy? Detect if our head is in the same cell as the candy
-    if (candy && candy.x === newHead.x && candy.y === newHead.y) {
-      candy = null;
+    
+    if (food && food.x === newHead.x && food.y === newHead.y) {
+      food = null;
       snakeLength += 20;
     }
 
-    context.fillStyle = '#002b36';
-    context.fillRect(0, 0, SIZE, SIZE); // Reset the play area
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, SIZE, SIZE); 
     if (end) {
-      context.fillStyle = '#eee8d5';
-      context.font = '40px serif';
+      context.fillStyle = 'red';
+      context.font = '20px sans-serif';
       context.textAlign = 'center';
-      context.fillText('Refresh to play again', SIZE / 2, SIZE / 2);
+      context.fillText('Click refresh to play the game again', SIZE / 2, SIZE / 2);
     } else {
-      snake.unshift(newHead); // Add the new head to the front
-      snake = snake.slice(0, snakeLength); // Enforce the snake's max length
+      snake.unshift(newHead); 
+      snake = snake.slice(0, snakeLength); 
     }
 
-    // Detect wall collisions
+    
     if (newHead.x < 0 || newHead.x >= SIZE || newHead.y < 0 || newHead.y >= SIZE) {
       end = true;
     }
 
-    context.fillStyle = '#268bd2';
+    context.fillStyle = 'green';
     var snakeObj = {};
     for (var i = 0; i < snake.length; i++) {
       var a = snake[i];
-      context.fillRect(a.x, a.y, GRID_SIZE, GRID_SIZE); // Paint the snake
-      // Build a collision lookup object
+      context.fillRect(a.x, a.y, GRID_SIZE, GRID_SIZE); 
+      
       if (i > 0) snakeObj[stringifyCoord(a)] = true;
     }
 
-    if (snakeObj[stringifyCoord(newHead)]) end = true; // Collided with our tail
+    if (snakeObj[stringifyCoord(newHead)]) end = true; 
 
-    // Place a candy (not on the snake) if needed
-    while (!candy || snakeObj[stringifyCoord(candy)]) {
-      candy = {x: randomOffset(), y: randomOffset()};
+    
+    while (!food || snakeObj[stringifyCoord(food)]) {
+      food = {x: randomOffset(), y: randomOffset()};
     }
 
     context.fillStyle = '#b58900';
-    context.fillRect(candy.x, candy.y, GRID_SIZE, GRID_SIZE); // Paint the candy
+    context.fillRect(food.x, food.y, GRID_SIZE, GRID_SIZE); 
   }
 
   window.onload = function() {
-    setInterval(tick, 100); // Kick off the game loop!
+    setInterval(tick, 100); 
     window.onkeydown = function(e) {
       newDirection = {37: -1, 38: -2, 39: 1, 40: 2}[e.keyCode] || newDirection;
     };
